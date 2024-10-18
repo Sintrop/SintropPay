90849490df1e3abe555b5b1d9e0a81cd480ebff2
Emanoel Augusto <emanoelaugusto7@gmail.com>
2024-10-18 15:10:52 -0300

    feat: config shadcn ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors:{
-        background: 'green',
-        container:{
-          primary: '#03364B',
-          secondary: '#012939',
-        },
-        green:{
-          primary: '#75D63A',
-          header: '#044640'
-        },
-        blue:{
-          primary: '#3E9EF5'
-        }
-      }
-    },
+  	extend: {
+  		colors: {
+  			background: 'green',
+  			container: {
+  				primary: '#03364B',
+  				secondary: '#012939'
+  			},
+  			green: {
+  				primary: '#75D63A',
+  				header: '#044640'
+  			},
+  			blue: {
+  				primary: '#3E9EF5'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
 
