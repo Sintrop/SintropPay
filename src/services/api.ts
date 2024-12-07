c612607cfcd731f29fcc7e0238cc6a4970bbd844
Emanoel Augusto <emanoelaugusto7@gmail.com>
2024-12-06 21:15:44 -0300

    feat: change api to v8

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'https://api-v7-production.up.railway.app',
+    baseURL: 'https://appapiv8.sintrop.com',
     //baseURL: 'http://localhost:3333'
 })
